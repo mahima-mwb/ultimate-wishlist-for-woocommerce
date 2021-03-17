2b59859cceab266d3f0db964411e9acca15c68da
Ashutosh Verma <ashutoshverma@cedcoss.com>
2021-03-17 13:28:49 +0530

    overview page completed

--- admin/partials/templates/wishlist-for-woo-overview.php
+++ admin/partials/templates/wishlist-for-woo-overview.php
@@ -1,1 +1,90 @@
-<h1>uskhbjbjhdsbf</h1>
+<ul class="wfw-overview_wrapper">
+	<li class="wfw-overview_banner">
+		<div class="wfw-overview_banner-image">
+			<img src="<?php echo esc_url( WISHLIST_FOR_WOO_URL . 'admin/images/bannerwish.bmp' ) ?>" alt="wfw-banner">
+		</div>
+	</li>
+	<li class="wfw-overview_content">
+		<div>
+			<h1>
+			What is a "Wishlist for WooCommerce"?</h1>
+			<p>
+				"Wishlist for WooCommerce" is a plugin that adds the wishlist feature to your WooCommerce store. With this plugin, your logged-in buyers get a specific page to save products they are interested in but can't purchase at the moment.
+
+				Shoppers can invite family and friends, and give them access to add or view the wishlist, or purchase the wishlist; making the gifting more convincing.
+
+				Overall, this plugin encourages and helps your buyers to wish for more and better. 
+			</p>
+			<h3>How Does This Plugin help?</h3>
+			<ul>
+				<li>Improve user's experience by letting them save products as wishlists.</li>
+				<li>Allows you to send push notifications directly to buyers reminding them of products in their wishlist</li>
+				<li>You get to know your buyer's interests and wishes.</li>
+				<li>You get to know the most liked product by tracking the product that is available in the majority of wishlists.</li>
+				<li>Connect wishlist with CRMs.</li>
+			</ul>
+		</div>
+	</li>
+	<li class="wfw-overview_card">
+		<h3>Free Features-</h3>
+			<ul>
+				<li class="wfw-overview_card-item">
+					<img src="<?php echo esc_url( WISHLIST_FOR_WOO_URL . 'admin/icons/Icons_Buyers.jpg' ) ?>" alt="wfw-icon">
+					<h4>
+						Buyers Can Create Wishlist
+					</h4>
+					<p>
+						Create one wishlist and add as many products that buyers wish for. Edit, remove, share and purchase the products added to the wishlist.
+					</p>
+				</li>
+				<li class="wfw-overview_card-item">
+					<img src="<?php echo esc_url( WISHLIST_FOR_WOO_URL . 'admin/icons/Icons_Shareable.jpg' ) ?>" alt="wfw-icon">
+					<h4>
+						Shareable Wishlist
+					</h4>
+					<p>
+						The created wishlist can be shared on social media such as Facebook, Twitter, Whatsapp, etc. Further, buyers can add their family and friends to their wishlist. 
+					</p>
+				</li>
+				<li class="wfw-overview_card-item">
+					<img src="<?php echo esc_url( WISHLIST_FOR_WOO_URL . 'admin/icons/Icons_Set.jpg' ) ?>" alt="wfw-icon">
+					<h4>
+						Set Wishlist Popup
+					</h4>
+					<p>
+						Set popup notification, that appears as a confirmation, when a product is added to the wishlist.
+					</p>
+				</li>
+			</ul>
+			<ul>
+				<li class="wfw-overview_card-item">
+					<img src="<?php echo esc_url( WISHLIST_FOR_WOO_URL . 'admin/icons/Icons_Customize.jpg' ) ?>" alt="wfw-icon">
+					<h4>
+					Customize Wishlist Button/Icon</h4>
+					<p>
+						Set the icon or button of how the "wishlist" feature is shown on the product listing or product page. Can be cart, heart, eye, tag, etc.
+					</p>
+				</li>
+				<li class="wfw-overview_card-item">
+					<img src="<?php echo esc_url( WISHLIST_FOR_WOO_URL . 'admin/icons/Icons_Purchase.jpg' ) ?>" alt="wfw-icon">
+					<h4>
+						Purchase Directly from Wishlist
+					</h4>
+					<p>
+						The products added to the wishlist have an "Add to Cart" button, that the users can use, and directly purchase the product from the wishlist.
+					</p>
+				</li>
+			</ul>
+	</li>
+	<li class="wfw-overview_support">
+		<h3>Exclusive Support</h3>
+		<p>
+			Receive dedicated 24*7 Phone, Email & Skype support. Our Support is ready to assist you regarding any query, issue, or feature request and if that doesn't help our Technical team will connect with you personally and have your query resolved.
+		</p>
+		<div class="wfw-overview_support-icon">
+			<a href="tel:+1(888)575-2397"><img src="<?php echo esc_url( WISHLIST_FOR_WOO_URL . 'admin/icons/phone-call.png' ) ?>" alt="phone"></a>
+			<a href="mailto:ticket@makewebbetter.com"><img src="<?php echo esc_url( WISHLIST_FOR_WOO_URL . 'admin/icons/mail.png' ) ?>" alt="mail"></a>
+			<a href="https://join.skype.com/invite/IKVeNkLHebpC"><img src="<?php echo esc_url( WISHLIST_FOR_WOO_URL . 'admin/icons/skype_logo.png' ) ?>" alt="wfw-skype"></a>
+		</div>
+	</li>
+</ul>
